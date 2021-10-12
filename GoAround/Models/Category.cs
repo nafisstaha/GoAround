7f0a84d39e967114d32f4874399e96e5b39613c7
nafisstaha <nafise.tahaei@gmail.com>
2021-10-13 03:27:14 +0330

    Category model

---
+++ GoAround/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoAround.Models
+{
+    public class Category
+    {
+        //get/set methods for category id and name
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
