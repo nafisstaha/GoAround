00f8f92ecf9778f931d2a70637812562b05d13cb
nafisstaha <nafise.tahaei@gmail.com>
2021-10-13 03:20:59 +0330

    Browse Category, displaying categories method, page titles

--- GoAround/Controllers/CategoriesController.cs
+++ GoAround/Controllers/CategoriesController.cs
@@ -12,5 +12,20 @@ namespace GoAround.Controllers
         {
             return View();
         }
+
+        //a method for displaying the list when the user click a category
+        public IActionResult Browse(string category)
+        {
+            //checking an empty or missed category and take back the user to category list
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //store the input parameter for displaying
+            ViewBag.category = category;
+
+            return View();
+        }
     }
 }
