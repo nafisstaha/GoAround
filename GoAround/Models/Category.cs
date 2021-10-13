4239d7f62984e48c4bdfd58a614c5f8e82e38af3
nafisstaha <nafise.tahaei@gmail.com>
2021-10-13 04:55:08 +0330

    Error message | Range and name for category id

--- GoAround/Models/Category.cs
+++ GoAround/Models/Category.cs
@@ -10,9 +10,14 @@ namespace GoAround.Models
     public class Category
     {
         //get/set methods for category id and name
+
+        //suitable range and name for CategoryId
+        [Range(1, 9999)]
+        [Display(Name = "Id")]
         public int CategoryId { get; set; }
 
-        [Required]
+        //Required name with a new error message
+        [Required(ErrorMessage = "We'll happy, if know the name of your place :)")]
         public string Name { get; set; }
     }
 }
